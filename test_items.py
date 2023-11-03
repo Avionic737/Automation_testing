fb28966a5be3a6d6efde1f9bb5a936de0cef6820
Avion737 <JuryG@mtnhausdigital.com>
2023-11-03 12:18:22 -0400

    add comment about timing

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 def test_add_to_cart_button_presence(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)  # Добавьте задержку
+    time.sleep(30)  # Добавление задержки
 
     # Проверяем наличие кнопки "Добавить в корзину" с помощью CSS-селектора
     add_to_cart_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
